c657e46ec62cda064a5258fd296621d04c1a1902
Angela Gilhotra <angelagilhotra@gmail.com>
2024-09-24 12:54:52 -0700

    add constants

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -8,12 +8,12 @@ import { Button } from "@burnt-labs/ui";
 
 export default function Page(): JSX.Element {
   // Abstraxion hooks
-  const allStuff = useAbstraxionAccount();
+  const stuff = useAbstraxionAccount();
 
   // General state hooks
   const [show, setShow] = useModal();
   console.log(show);
-  console.log(allStuff)
+  console.log(stuff)
   return (
       <main className="m-auto flex min-h-screen max-w-xs flex-col items-center justify-center gap-4 p-4">
         <h1 className="text-2xl font-bold tracking-tighter text-black dark:text-white">
@@ -24,12 +24,25 @@ export default function Page(): JSX.Element {
             onClick={() => { setShow(true) }}
             structure="base"
         >
-          {allStuff.data.bech32Address ? (
+          {stuff.data.bech32Address ? (
               <div className="flex items-center justify-center">VIEW ACCOUNT</div>
           ) : (
               "CONNECT"
           )}
         </Button>
+        {
+          stuff.data.bech32Address &&
+            <div className="border-2 border-black rounded-md p-4 flex flex-row gap-4">
+              <div className="flex flex-row gap-6">
+                <div>
+                  address
+                </div>
+                <div>
+                  {stuff.data.bech32Address}
+                </div>
+              </div>
+            </div>
+        }
         <Abstraxion onClose={() => setShow(false)} />
       </main>
   );
