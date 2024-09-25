b31befebea5a05fe4b69b59404543957c69b89be
Angela Gilhotra <angelagilhotra@gmail.com>
2024-09-25 12:50:10 -0700

    smol fix

--- web-app/src/utils/constants.ts
+++ web-app/src/utils/constants.ts
@@ -2,5 +2,6 @@ export const contracts = [
   'xion18gn46ys5jtc07vlaq5xk5u7p7nhzauuphy0z5netvn7dhkuwv2mse7l3ul' // potato contract
 ]
 
-export const EXPLORER = 'https://explorer.burnt.com'
 export const CHAIN_ID = 'xion-testnet-1'
+export const EXPLORER = `https://explorer.burnt.com/${CHAIN_ID}`
+
