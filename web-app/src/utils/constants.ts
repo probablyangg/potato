c264873edcb85648c8e2bb47e6591c62795101c6
Angela Gilhotra <angelagilhotra@gmail.com>
2024-10-07 13:25:39 -0700

    Add treasury contract in constants

--- web-app/src/utils/constants.ts
+++ web-app/src/utils/constants.ts
@@ -2,7 +2,12 @@ export const CONTRACTS = {
   'potato': 'xion1v6476wrjmw8fhsh20rl4h6jadeh5sdvlhrt8jyk2szrl3pdj4musyxj6gl'
 }
 
+export const TREASURY = {
+  treasury: 'xion1gqwtme9ekrrn3vqny352hs22vslx8ea7wuy557lwyjaallqeda9s5ld60e'
+}
+
 export const CONTRACTS_ARRAY = Object.values(CONTRACTS)
+
 
 export const CHAIN_ID = 'xion-testnet-1'
 
